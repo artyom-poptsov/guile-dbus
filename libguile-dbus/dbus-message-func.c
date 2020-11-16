f0796e61b22c2864516581f8a142c9c8b73d574d
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2020-11-16 22:35:19 +0300

    message.scm: Add new procedures
    * libguile-dbus/dbus-message-func.c (gdbus_messsage_new_method_return)
      (gdbus_messsage_new_signal): New procedures.
    * modules/dbus/message.scm (make-dbus-message/method-return)
      (make-dbus-message/signal): New procedures.

--- libguile-dbus/dbus-message-func.c
+++ libguile-dbus/dbus-message-func.c
@@ -47,7 +47,6 @@ GDBUS_DEFINE(gdbus_message_new_method_call, "%make-dbus-message/method-call", 4,
 #undef FUNC_NAME
 
 
-
 GDBUS_DEFINE(gdbus_message_new_error, "%make-dbus-message/error", 3,
              (SCM message, SCM error_name, SCM error_message),
              "Make a new message that is a reply to another message.")
@@ -75,6 +74,55 @@ GDBUS_DEFINE(gdbus_message_new_error, "%make-dbus-message/error", 3,
     }
 
     scm_dynwind_end();
+    return _scm_from_dbus_message(new_message);
+}
+#undef FUNC_NAME
+
+GDBUS_DEFINE(gdbus_messsage_new_method_return,
+             "%make-dbus-message/method-return", 1,
+             (SCM method_call),
+             "Constructs a message that is a reply to a method call.")
+#define FUNC_NAME s_gdbus_messsage_new_method_return
+{
+    struct dbus_message_data* data = _scm_to_dbus_message_data(method_call);
+    DBusMessage* new_message = dbus_message_new_method_return(data->message);
+    if (new_message == NULL) {
+        gdbus_error(FUNC_NAME, "Could not create a message", method_call);
+    }
+
+    return _scm_from_dbus_message(new_message);
+}
+#undef FUNC_NAME
+
+GDBUS_DEFINE(gdbus_messsage_new_signal, "%make-dbus-message/signal", 3,
+             (SCM path, SCM iface, SCM name),
+             "Constructs a new message representing a signal emission.")
+#define FUNC_NAME s_gdbus_messsage_new_signal
+{
+    DBusMessage* new_message = NULL;
+    char* c_path  = NULL;
+    char* c_iface = NULL;
+    char* c_name  = NULL;
+
+    scm_dynwind_begin(0);
+
+    c_path = scm_to_locale_string(path);
+    scm_dynwind_free(c_path);
+
+    c_iface = scm_to_locale_string(iface);
+    scm_dynwind_free(c_iface);
+
+    c_name = scm_to_locale_string(name);
+    scm_dynwind_free(c_name);
+
+    new_message = dbus_message_new_signal(c_path, c_iface, c_name);
+    if (new_message == NULL) {
+        gdbus_error(FUNC_NAME, "Could not create a message",
+                    scm_list_3(path, iface, name));
+    }
+
+    scm_dynwind_end();
+
     return _scm_from_dbus_message(new_message);
 }
 #undef FUNC_NAME
--- modules/dbus/message.scm
+++ modules/dbus/message.scm
@@ -3,6 +3,8 @@
             dbus-message?
             make-dbus-message
             make-dbus-message/method-call
+            make-dbus-message/method-return
+            make-dbus-message/signal
             make-dbus-message/error))
 
 
@@ -16,11 +18,22 @@
                                         method)
   (%dbus-message-new-method-call destination path iface method))
 
+(define (make-dbus-message/method-return message)
+  (%make-dbus-message/method-return message))
+
+(define (make-dbus-message/signal #:key
+                                  path
+                                  iface
+                                  name)
+  (%make-dbus-message/signal path iface name))
+
 (define* (make-dbus-message/error #:key
                                   message
                                   error-name
                                   error-message)
   (%make-dbus-message/error message error-name error-message))
+
+
 
 
 (load-extension "libguile-dbus" "init_dbus_message")
