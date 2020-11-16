5e07496cc113143133a064f3a435d3a8b61073a9
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2020-11-16 19:56:19 +0300

    message (make-dbus-message/method-call): New procedure
    * modules/dbus/message.scm (make-dbus-message/method-call): New procedure.
    * libguile-dbus/dbus-message-func.c: Add to the repository.
    * libguile-dbus/Makefile.am: Update.

---
+++ libguile-dbus/dbus-message-func.c
@@ -0,0 +1,44 @@
+#include <dbus/dbus.h>
+#include <libguile.h>
+
+#include "dbus-message-type.h"
+
+SCM_DEFINE(gdbus_message_new_method_call,
+           "%make-dbus-message/method-call", 4, 0, 0,
+           (SCM destination, SCM path, SCM iface, SCM method),
+           "Make a new message to invoke a method on a remote object.")
+#define FUNC_NAME s_gdbus_message_new_method_call
+{
+    char* c_destination;
+    char* c_path;
+    char* c_iface;
+    char* c_method;
+    DBusMessage* message;
+
+    scm_dynwind_begin(0);
+
+    c_destination = scm_to_locale_string(destination);
+    scm_dynwind_free(c_destination);
+
+    c_path = scm_to_locale_string(path);
+    scm_dynwind_free(c_path);
+
+    c_iface = scm_to_locale_string(iface);
+    scm_dynwind_free(c_iface);
+
+    c_method = scm_to_locale_string(method);
+    scm_dynwind_free(c_method);
+
+    message = dbus_message_new_method_call(c_destination,
+                                           c_path,
+                                           c_iface,
+                                           c_method);
+    if (message == NULL) {
+        /* TODO: Add error checking */
+    }
+
+    scm_dynwind_end();
+
+    return _scm_from_dbus_message(message);
+}
+#undef FUNC_NAME
--- modules/dbus/message.scm
+++ modules/dbus/message.scm
@@ -1,11 +1,19 @@
 (define-module (dbus message)
   #:export (dbus-message
+            dbus-message?
             make-dbus-message
-            dbus-message?))
+            make-dbus-message/method-call))
 
 
 (define* (make-dbus-message #:key (type 'invalid))
   (%make-dbus-message type))
+
+(define* (make-dbus-message/method-call #:key
+                                        destination
+                                        path
+                                        iface
+                                        method)
+  (%dbus-message-new-method-call destination path iface method))
 
 
 (load-extension "libguile-dbus" "init_dbus_message")
