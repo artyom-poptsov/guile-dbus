59c743f7183e31c7318880789fed50ba50335f7b
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2020-11-22 00:08:41 +0300

    pending-call.scm (dbus-pending-call-cancel): New procedure
    * modules/dbus/pending-call.scm (dbus-pending-call-cancel): New procedure.
    * libguile-dbus/dbus-pending-call-func.c (gdbus_pending_call_cancel): New
      procedure.

--- libguile-dbus/dbus-pending-call-func.c
+++ libguile-dbus/dbus-pending-call-func.c
@@ -15,6 +15,19 @@ GDBUS_DEFINE(gdbus_pending_call_block, "dbus-pending-call-block", 1,
 }
 #undef FUNC_NAME
 
+GDBUS_DEFINE(gdbus_pending_call_cancel, "dbus-pending-call-cancel", 1,
+             (SCM call),
+             "\
+Cancels the pending call, such that any reply \
+or error received will just be ignored.")
+#define FUNC_NAME s_gdbus_pending_call_cancel
+{
+    gdbus_pending_call_t* gdbus_call = gdbus_pending_call_from_scm(call);
+    dbus_pending_call_cancel(gdbus_call->call);
+    return SCM_UNDEFINED;
+}
+#undef FUNC_NAME
+
 
 void init_dbus_pending_call_func()
 {
--- modules/dbus/pending-call.scm
+++ modules/dbus/pending-call.scm
@@ -1,6 +1,7 @@
 (define-module (dbus pending-call)
   #:export (dbus-pending-call
-            dbus-pending-call-block))
+            dbus-pending-call-block
+            dbus-pending-call-cancel))
 
 
 (load-extension "libguile-dbus" "init_dbus_pending_call")
